333bee14da3096520c3cb2b3904edc80351783d2
facavc01-oss <facavc01@gmail.com>
2026-04-06 22:53:56 -0300

    feat: initialize Supabase client with project credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eebexwcdmgyidtsjrsff.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlYmV4d2NkbWd5aWR0c2pyc2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMzcyNjEsImV4cCI6MjA5MDgxMzI2MX0.u-l3cG1l5yekyWqIG_SioDd8acj53fHasYkJwwFO63w';
+const supabaseUrl = 'https://ghtjqjenlyvtnivvbxdq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodGpxamVubHl2dG5pdnZieGRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTgzNDksImV4cCI6MjA5MTA5NDM0OX0.rNIV1TyJQ3UNtll-dcBIGCSYT-feezZJy1hEfgwrMLw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
